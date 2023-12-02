2f09148ca24b98a298b0d92a4c816a5dc1e8d7ea
0929230515jimmy <93629211+0929230515jimmy@users.noreply.github.com>
2023-12-02 16:39:49 -0500

    12/2

--- question3a.py
+++ question3a.py
@@ -1,9 +1,9 @@
 import scipy.stats as stats
+intercept = 0.77
+slope_k = -0.74
+slope_bb = 1.45
 
 def calculator(k_percentage, bb_percentage):
-    intercept = 0.77
-    slope_k = -0.74
-    slope_bb = 1.45
     residual_std_error = 0.220
     ops = intercept + slope_k * k_percentage + slope_bb * bb_percentage
     return ops 
@@ -11,5 +11,5 @@ def calculator(k_percentage, bb_percentage):
 batter_a = calculator(0.2,0.2)
 batter_b = calculator(0.05,0.1)
 
-print(batter_a)
-print(batter_b)
+probability = 1 - stats.norm.cdf(batter_b - batter_a, loc=0, scale=0.22)
+print(probability)
---
+++ question3b.py
@@ -0,0 +1,11 @@
+import math
+
+intercept = -0.63
+slope_k = -6.52
+slope_bb = 13.7
+
+k_c = 0.20
+bb_c = 0.14
+
+k_d = 0.25
+bb_d = 0.20
