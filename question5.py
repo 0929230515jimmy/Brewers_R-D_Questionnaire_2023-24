515c5b5efea1b795dd0ff4ffdd0518e74b943173
0929230515jimmy <93629211+0929230515jimmy@users.noreply.github.com>
2023-12-02 23:47:03 -0500

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,44 +1,36 @@
 import random
 
-game = ['B'] * 10 + ['K'] * 26 + ['H'] * 4
-
-random.shuffle(game)
-game.append("K")
-
-game1 = ['B', 'K', 'K', 'K', 'K', 'B', 'K', 'B', 'K', 'K', 'K', 'K', 'K', 'B', 'K', 'K', 'K', 'K', 'H', 'K', 'K', 'K', 'H', 'B', 'K', 'K', 'B', 'H', 'B', 'K', 'K', 'K', 'K', 'B', 'K', 'K', 'B', 'K', 'B', 'H', 'K']
-
 def expected_run(n):
     k = 0
     baserunner = 0
     runs = 0
     total_run = []
-    for i in range(40):
-        if k == 3:    
-            total_run.append(runs)
-            runs = 0
-            k = 0
-            baserunner = 0
-            print(i, k, baserunner, runs, total_run)
-        elif n[i] == "K":
+    for i in range(41):
+        if n[i] == "K":
             k += 1
-            print(i, k, baserunner, runs, total_run)
+            if k == 3:    
+                total_run.append(runs)
+                runs = 0
+                k = 0
+                baserunner = 0
         elif n[i] == "B":
             if baserunner == 3:
                 runs += 1
                 baserunner = 3
-                print(i, k, baserunner, runs, total_run)
             else:
                 baserunner += 1
-                print(i, k, baserunner, runs, total_run)
         elif n[i] == "H":   
             runs = baserunner + 1
             baserunner = 0
-            print(i, k, baserunner, runs, total_run)
-    return total_run
+    sum_run = sum(total_run)
+    return sum_run
     
-#train = 10000
-#runs = 0
-#for i in range train:
-    #expe
-#print (game1)
-print(expected_run(game1))
+train = 10000
+sum_of_run = 0
+for i in range (train):
+    game = ['B'] * 10 + ['K'] * 26 + ['H'] * 4
+    random.shuffle(game)
+    game.append("K")
+    sum_of_run += expected_run(game)
+
+print(sum_of_run/(train*9))
